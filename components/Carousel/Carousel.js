caca3d751075d11e852eb02537a561250a2ae2fe
Giacomo Benati <giacomobenati@mailbox.org>
2019-02-01 10:59:11 +0100

    (STRETCH) carousel with infinite loop functionality  added

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,8 +1,45 @@
 class Carousel {
+    constructor (carousel) {
+        this.carousel = carousel;
+        this.leftButton = this.carousel.querySelector('.left-button');
+        this.rightButton = this.carousel.querySelector('.right-button');
+        this.images = this.carousel.querySelectorAll('img');
+        this.index = 0;
 
+        this.selectImage();
+        this.rightButton.addEventListener('click', () => {
+            this.selectImage();
+            if (this.index === 3) {
+                this.index = 0;
+                this.selectImage();
+            } else {
+                this.index++;
+                this.selectImage();
+            }
+        })
+
+        this.leftButton.addEventListener('click', () => {
+            this.selectImage();
+            if (this.index === 0) {
+                this.index = 3;
+                this.selectImage();
+            } else {
+                this.index--;
+                this.selectImage();
+            }
+        })
+        
+    }
+    
+    selectImage() {
+        this.images[this.index].classList.toggle('selected-img');
+    }
 }
 
-let carousel = document.querySelector();
+
+
+let carousel = document.querySelector('.carousel');
+new Carousel(carousel);
 
 /* If You've gotten this far, you're on your own! Although we will give you some hints:
     1. You will need to grab a reference to the carousel, and in it grab the laft and right buttons
